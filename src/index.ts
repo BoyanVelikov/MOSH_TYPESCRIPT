22abc51c9064225557fd1129bc6514b1a65c2ab7
Boyan Velikov <boyanvelikov@gmail.com>
2022-10-22 09:09:36 +0300

    22-10-2022

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-let age: number = 20;
+// let sales = 123_456_789;
+// let course = 'TypeScript';
+// let is_published = true;
+// let level;
 
-if(age < 50) 
- age += 10;
+// import { toASCII } from "punycode";
 
-console.log(age);
+// function render(document) {
+//     console.log(render);
+// }
+
+// let numbers: number[] = [1.1111, 2.22222];
+// console.log(numbers.forEach(n => n.toFixed(2)));
+
+//1, 'Mosh'
+
+// let user: [number, string, boolean, number] = [1, 'Mosh', true, 5];
+// user.push(1)
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+//PascalCase
+
+enum Size {Small = 1, Medium, Large}
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
